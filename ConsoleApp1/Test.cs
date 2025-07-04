1e289326a49b0020c322334d224853d184fb755c
med2mad <mohamed.leghdaich@gmail.com>
2025-07-04 21:06:57 +0100

    init

---
+++ ConsoleApp1/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qeqwqewqe;
+
+public class Test
+{
+    public string zzz { get; set; }
+    public int zzzz { get; set; }
+    public int zzzzzzz { get; set; }
+}
+
+public enum en1
+{
+
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace TestProject1;
+
+
+    public class UnitTest1
+    {
+    public qeqwqewqe.Test D { get; set; } = new();
+
+    [Fact]
+    public void Test1()
+    {
+
+        Assert.Equal(5, 5);
+
+        
+        
+    }
+}
+
